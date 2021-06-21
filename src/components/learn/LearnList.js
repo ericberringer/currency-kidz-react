3bd23def5dc9fe0a52f7538cd53bee31b1eade90
ericberringer <eric.berringer@gmail.com>
2021-06-21 17:59:44 -0500

    build: learn page structure and responsiveness is about complete.

--- src/components/learn/LearnList.js
+++ src/components/learn/LearnList.js
@@ -13,7 +13,10 @@ import Image from 'react-bootstrap/Image'
 import Milo from './images/Milo.png'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 import './Learn.css'
 import React, { useContext, useEffect } from "react"
 import { ProfileContext } from "../profile/ProfileProvider"
@@ -34,50 +37,149 @@ export const LearnList = () => {
                     <img className="milo image" alt="milo profile" src={Milo}></img>
                 </div>
                     <h1>Hey {profile.saver?.user.first_name}!!</h1>
-                    <h2>Let's Select a Coin</h2>
+                    <h2>Let's Learn About Our Money</h2>
             </article>
-            <Container className="learnCoinContainer">
-                <Row className="learnCoins">
-                    <Col className="learnCol">
-                        <Image className="learnImg learnCoinImg learnPenny" alt="money" src={Penny} />
-                    </Col>
-                    <Col className="learnCol">
-                        <Image className="learnImg learnCoinImg learnNickel" alt="money" src={Nickel} />
-                    </Col>
-                    <Col className="learnCol">
-                        <Image className="learnImg learnCoinImg learnDime" alt="money" src={Dime} />
-                    </Col>
-                    <Col className="learnCol">
-                        <Image className="learnImg learnCoinImg learnQuarter" alt="money" src={Quarter} />
-                    </Col>
-                </Row>
-                <div>
-                    
-                </div>
+            <Container fluid>
+                <article className="learnArticle">
                 <Row>
                     <Col className="learnCol">
-                        <Image className="learnImg learnBillImg learnDollar" alt="money" src={Dollar} />
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="0">
+                            <Image className="learnCoinImg" alt="money" src={Penny} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <div>This is a penny</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
                     </Col>
                     <Col className="learnCol">
-                        <Image className="learnImg learnBillImg learnFive" alt="money" src={Five} />
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="1">
+                            <Image className="learnCoinImg" alt="money" src={Nickel} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                                <div>This is a nickel</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
                     </Col>
                 </Row>
-                <Row className="middleRowBillsLearn">
-                    <Col className="learnCol">
-                        <Image className="learnImg learnBillImg learnTen" alt="money" src={Ten} />
-                    </Col>
-                    <Col className="learnCol">
-                        <Image className="learnImg learnBillImg learnTwenty" alt="money" src={Twenty} />
-                    </Col>
-                </Row>
+                </article>
+                <article className="learnArticle">
                 <Row>
                     <Col className="learnCol">
-                        <Image className="learnImg learnBillImg learnFifty" alt="money" src={Fifty} />
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="0">
+                            <Image className="learnCoinImg" alt="money" src={Dime} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <div>This is a dime</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
                     </Col>
                     <Col className="learnCol">
-                        <Image className="learnImg learnBillImg learnHundred" alt="money" src={Hundred} />
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="1">
+                            <Image className="learnCoinImg" alt="money" src={Quarter} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                                <div>This is a quarter</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
                     </Col>
                 </Row>
+                </article>
+                <article className="learnArticle">
+                <Row>
+                    <Col className="learnCol">
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="0">
+                            <Image className="learnBillImg" alt="money" src={Dollar} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <div>This is a dollar</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
+                    </Col>
+                    <Col className="learnCol">
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="1">
+                            <Image className="learnBillImg" alt="money" src={Five} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                                <div>This is five dollars</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
+                    </Col>
+                </Row>
+                </article>
+                <article className="learnArticle">
+                <Row>
+                    <Col className="learnCol">
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="0">
+                            <Image className="learnBillImg" alt="money" src={Ten} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <div>This is ten dollars</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
+                    </Col>
+                    <Col className="learnCol">
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="1">
+                            <Image className="learnBillImg" alt="money" src={Twenty} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                                <div>This is a twenty dollars</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
+                    </Col>
+                </Row>
+                </article>
+                <article className="learnArticle">
+                <Row>
+                    <Col className="learnCol">
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="0">
+                            <Image className="learnBillImg" alt="money" src={Fifty} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <div>This is fifty dollars</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
+                    </Col>
+                    <Col className="learnCol">
+                    <Accordion>
+                        <Accordion.Toggle as={Col} variant="link" eventKey="1">
+                            <Image className="learnBillImg" alt="money" src={Hundred} />
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>
+                                <div>This is one hundred dollars</div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Accordion>
+                    </Col>
+                </Row>
+                </article>
             </Container>
         </section>
         </>
