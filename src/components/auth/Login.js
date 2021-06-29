4959784c185bd3795a5ccede08969b03d9648d54
ericberringer <eric.berringer@gmail.com>
2021-06-29 12:00:22 -0500

    Now the login fetch is fixed.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -14,7 +14,7 @@ export const Login = () => {
     const handleLogin = (e) => {
         e.preventDefault()
 
-        return fetch("http://localhost:8000/login", {
+        return fetch("https://currency-kidz.herokuapp.com/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
