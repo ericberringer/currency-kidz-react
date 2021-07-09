25ad499fd8e934386137ec2fa13b0dcc49658672
ericberringer <eric.berringer@gmail.com>
2021-07-09 12:49:06 -0500

    build: parse floated and tofixed(2) total deposits on the withdrawal page.

--- src/components/withdrawals/WithdrawalList.js
+++ src/components/withdrawals/WithdrawalList.js
@@ -143,7 +143,7 @@ export const WithdrawalList = () => {
                 <img className="milo image" alt="milo profile" src={Milo}></img>
             </div>
                 <h1 className="impact">Hey {profile.saver?.user.first_name}!</h1>
-                <h3 className="impact">You currenty have ${totalDeposits}!</h3>
+                <h3 className="impact currentDepositTitle2">You currenty have ${parseFloat(totalDeposits.toFixed(2))}!</h3>
                 <h2 className="impact">How much money would you like back?</h2>
                 <div className="depositHeaderDiv">
                     <div className="depositPiggyDiv">
